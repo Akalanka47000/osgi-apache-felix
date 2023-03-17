41872028db6cd901fe0d22340d993a18a4111426
Devshe <sheroniperera20@gmail.com>
2023-03-17 21:53:31 +0530

    changed fileWriter activator

--- file-writer/src/file_writer/Activator.java
+++ file-writer/src/file_writer/Activator.java
@@ -2,19 +2,23 @@ package file_writer;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
-	private FileWriterImpl fileWriterImpl;
+	ServiceRegistration fileWriterRegistration;
 	
 	public void start(BundleContext context) throws Exception {
-		fileWriterImpl = new FileWriterImpl();
-		context.registerService(FileWriterService.class.getName(),fileWriterImpl, null);
-		fileWriterImpl.start(context);
+		System.out.println("File-writer started");
+		FileWriterService fileWriterService = new FileWriterImpl();
+		
+		fileWriterRegistration = context.registerService(FileWriterService.class.getName(), fileWriterService, null);
+		
 	}
 
 	public void stop(BundleContext context) throws Exception {
-		fileWriterImpl.stop(context);
+		System.out.println("file-writer Stopped");
+		fileWriterRegistration.unregister();
 	}
 
 }
--- module-logger/src/osgi/logger/Activator.java
+++ module-logger/src/osgi/logger/Activator.java
@@ -6,17 +6,13 @@ import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator
 {
-	private static BundleContext context;
+	
 	ServiceRegistration loggerServiceRegistration;
 	
-	static BundleContext getContext() {
-		return context;
-		
-	}
-
 	public void start(BundleContext bundleContext) throws Exception {
 		System.out.println("Logger started");
 		LoggerService loggerService = new LoggerServiceImpl();
+		
 		loggerServiceRegistration = bundleContext.registerService(LoggerService.class.getName(), loggerService, null);
 	}
 
