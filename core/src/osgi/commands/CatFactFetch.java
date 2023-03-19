ee22f495e0804f130cd4069a067cc3aead216900
QbicSquid <shifan611@gmail.com>
2023-03-19 23:18:42 +0530

    Added console printing to core

--- core/src/osgi/commands/CatFactFetch.java
+++ core/src/osgi/commands/CatFactFetch.java
@@ -9,14 +9,17 @@ import osgi.command.ICommand;
 import osgi.http.client.HttpClient;
 import osgi.http.client.HttpResponse;
 import osgi.logger.LoggerService;
+import osgi.outputhandler.OutputHandlerService;
 
 public class CatFactFetch implements ICommand, Serializable {
 	private LoggerService logger;
+	private OutputHandlerService output;
 	private BundleContext context;
 	
 	public CatFactFetch(BundleContext context) {
 		this.context = context;
 		
+		// get the module logger
 		ServiceTracker<?, ?> tracker = null;
 		try {
 			 tracker = new ServiceTracker<LoggerService, LoggerService>(
@@ -26,6 +29,20 @@ public class CatFactFetch implements ICommand, Serializable {
 					null);
 			tracker.open();
 			logger = (LoggerService) tracker.getService();
+			tracker.close();
+		} catch (Exception e) {
+			System.out.println("ERROR");
+		}
+
+		// get output handler service
+		try {
+			 tracker = new ServiceTracker<LoggerService, LoggerService>(
+					context,
+					context.createFilter(
+							"(&(objectClass=" + OutputHandlerService.class.getName() + "))"),
+					null);
+			tracker.open();
+			output = (OutputHandlerService) tracker.getService();
 			tracker.close();
 		} catch (Exception e) {
 			System.out.println("ERROR");
@@ -48,6 +65,8 @@ public class CatFactFetch implements ICommand, Serializable {
         HttpResponse res = client.get("https://catfact.ninja/fact");
         System.out.println(res.getStatus());
         System.out.println(res.getData());
+
+        output.PrintWithBorder((String) res.getData().get("message"));
 	}
 
 	@Override
